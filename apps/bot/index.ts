0a3f9e9cb8222b9aa606b991262fd1f5524f3f72
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-10-28 14:48:33 -0400

    add command handlers

--- apps/bot/index.ts
+++ apps/bot/index.ts
@@ -2,6 +2,7 @@ import { currencyTable } from "@repo/database/schema"
 import { commands } from "./commands"
 import { db } from "@repo/database"
 import { Client } from "discord.js";
+import { Command } from "./commands/types";
 
 const client = new Client({
     intents: [
@@ -9,8 +10,24 @@ const client = new Client({
     ]
 })
 
+const commandMap: { [key: string]: Command } = {}
+for (const command of commands) {
+    commandMap[command.json.name] = command
+}
+
 client.on('ready', () => {
     console.log('Bot is ready')
+})
+
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isCommand()) {
+        try {
+            const command = commandMap[interaction.commandName]
+            await command.run(interaction)
+        } catch (e) {
+            interaction.reply('An error occurred')
+        }
+    }
 })
 
 const currencies = await db.select().from(currencyTable)

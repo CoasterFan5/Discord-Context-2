c2b388e18069b614bccad8085ba7509f77ceeea8
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-10-28 14:58:59 -0400

    font

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '@fontsource-variable/inter';
+</script>
+
+<slot />
+
+<style lang="scss">
+	:global(body) {
+		margin: 0px;
+		padding: 0px;
+		font-family: 'Inter', sans-serif;
+	}
+</style>
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="wrap">
+	<h1>Welcome to Discord Context</h1>
+</div>
